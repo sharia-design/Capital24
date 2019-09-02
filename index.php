ed390d3a38fcf157e6da3634c0137051bcc1b012
Saba Sharia <shariasaba@gmail.com>
2019-09-02 22:49:07 +0400

    Upload First

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Capital 24 | Make Money</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="favicon.ico">
+        <link rel="icon" href="favicon.ico">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+    	<div class="fx-blue"></div>
+    	<div class="fx-yellow"></div>
+
+    	<div class="wrap">
+	    	<header class="header">
+		    	<img class="logo" src="img/logo-white.png" alt="Logo" height="52">
+		    	<nav class="nav">
+		    		<a href="#Intro">Intro</a>
+		    		<a href="#">How</a>
+		    		<a href="#" class="active">About</a>
+		    		<a href="#">Contact</a>
+		    	</nav>
+		    	<div class="header-btn">Sign Up</div>
+	    	</header><!-- /header -->
+    	</div>
+
+        <!-- Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="js/app.js"></script>
+    </body>
+</html>
